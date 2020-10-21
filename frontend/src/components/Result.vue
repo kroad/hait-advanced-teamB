c6a2b6c359c7e332969a83793e43c30169acd319
taillong <63985597+taillong@users.noreply.github.com>
2020-10-21 16:52:56 +0900

    コンポーネントに分ける

---
+++ frontend/src/components/Result.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>結果</h2>
+    <div v-for="artist in artists" :key="artist">
+      <p>アーティスト：{{ artist }}</p>
+      <div v-for="song in songs" :key="song.id">
+        <template v-if="song.artist_name === artist">
+          <p>曲名：{{ song.title }}</p>
+          <p>最高音：{{ song.heighest_name }}</p>
+          <p>最低音：{{ song.lowest_name }}</p>
+          <hr />
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    artists: {
+      type: Array,
+    },
+    songs: {
+      type: Array,
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/SongForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div>
+      <label for="artist1">歌手1</label>
+      <input id="artist1" type="number" v-model.number="info.artist1" />
+    </div>
+    <div>
+      <label for="artist2">歌手2</label>
+      <input id="artist2" type="number" v-model.number="info.artist2" />
+    </div>
+    <div>
+      <label for="heighest">最高音</label>
+      <input id="heighest" type="number" v-model.number="info.heighest" />
+    </div>
+    <div>
+      <label for="lowest">最低音</label>
+      <input id="lowest" type="number" v-model.number="info.lowest" />
+    </div>
+    <button v-on:click="sendToSongApi">APIへリクエストを送る</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      info: {
+        artist1: 1,
+        artist2: null,
+        heighest: 84,
+        lowest: 1,
+      },
+      songs: [],
+    };
+  },
+  computed: {
+    artists: function () {
+      let artists = [];
+      for (let song of this.songs) {
+        if (!artists.includes(song.artist_name)) {
+          artists.push(song.artist_name);
+        }
+      }
+      return artists;
+    },
+  },
+  methods: {
+    sendToSongApi() {
+      axios
+        .get("http://127.0.0.1:8000/api/v1/songs/", {
+          params: {
+            artist1: this.info.artist1,
+            artist2: this.info.artist2,
+            heighest: this.info.heighest,
+            lowest: this.info.lowest,
+          },
+        })
+        .then((response) => {
+          this.songs = response.data;
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
