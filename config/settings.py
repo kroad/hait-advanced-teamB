fff9e1400b33ce14d3b2b34ae543e9cc4188220e
kroad <54136336+kroad@users.noreply.github.com>
2020-10-23 23:26:35 +0900

    ブランチ合わせて調節した

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'corsheaders',
     'apiv1.apps.Apiv1Config',
     'karaoke.apps.KaraokeConfig',
