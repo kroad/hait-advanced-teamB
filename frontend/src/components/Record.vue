d47923eaf2712716933f581e356bfc58fc0a0ebe
taillong <63985597+taillong@users.noreply.github.com>
2020-10-25 17:55:10 +0900

    Update Record.vue

--- frontend/src/components/Record.vue
+++ frontend/src/components/Record.vue
@@ -1,10 +1,85 @@
 <template>
   <div>
-    <h1>recordしますよ</h1>
-    <input type="file" accept="audio/*" capture="microphone" />
+    <input
+      type="file"
+      accept="audio/*"
+      capture="microphone"
+      id="recorder"
+      @change="playSound"
+    />
+    <button @click="record">録音</button>
+    <audio id="player" controls :src="source"></audio>
+    <button @click="speaker">音を鳴らす</button>
+
+    <p>{{ source }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      source: "",
+    };
+  },
+  methods: {
+    speaker() {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      // Create the instance of AudioContext
+      let context = new AudioContext();
+      // Create the instance of OscillatorNode
+      let oscillator = context.createOscillator();
+      // for legacy browsers
+      oscillator.start = oscillator.start || oscillator.noteOn;
+      oscillator.stop = oscillator.stop || oscillator.noteOff;
+      // for legacy browsers
+      context.createGain = context.createGain || context.createGainNode;
+      // Create the instance of GainNode
+      let gain = context.createGain();
+      // OscillatorNode (Input) -> GainNode (Volume) -> AudioDestinationNode (Output)
+      oscillator.connect(gain);
+      gain.connect(context.destination);
+      // Start sound
+      oscillator.start(0);
+      // Stop sound (after 5 sec)
+      window.setTimeout(function () {
+        oscillator.stop(0);
+      }, 3000);
+      // 音量調整
+      gain.gain.value = 0.1;
+    },
+
+    playSound(event) {
+      let file = event.target.files[0];
+      this.source = URL.createObjectURL(file);
+    },
+
+    record() {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true, video: false })
+        .then((stream) => {
+          let recorder = new MediaRecorder(stream);
+          let chunks = [];
+          recorder.addEventListener("dataavailable", (e) => {
+            chunks.push(e.data);
+            console.log(e);
+            console.log(chunks);
+          });
+          recorder.addEventListener("stop", () => {
+            const blob = new Blob(chunks, { type: "audio/mp3" });
+            this.source = window.URL.createObjectURL(blob);
+            console.log(blob);
+          });
+          recorder.start(1000);
+          setTimeout(() => {
+            recorder.stop();
+          }, 5000);
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
