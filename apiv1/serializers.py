10cab3b5b473897897b4917b49c9069de992af19
taillong <63985597+taillong@users.noreply.github.com>
2020-10-29 13:26:38 +0900

    音階の表記を変える
    djangoのmodelを書き換えた

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -2,12 +2,26 @@ from rest_framework import serializers
 
 from karaoke.models import Song
 
+
 class SongSerializer(serializers.ModelSerializer):
-    
-    artist_name = serializers.ReadOnlyField(source='artist.name')
-    heighest_name = serializers.ReadOnlyField(source='heighest.name')
-    lowest_name = serializers.ReadOnlyField(source='lowest.name')
+
+    artist_name = serializers.ReadOnlyField(source="artist.name")
+    heighest_japan = serializers.ReadOnlyField(source="heighest.japan")
+    heighest_universal = serializers.ReadOnlyField(source="heighest.universal")
+    lowest_japan = serializers.ReadOnlyField(source="lowest.japan")
+    lowest_universal = serializers.ReadOnlyField(source="lowest.universal")
 
     class Meta:
         model = Song
-        fields = ['id','title','artist_name','heighest_name','lowest_name','artist','heighest','lowest']
+        fields = [
+            "id",
+            "title",
+            "artist_name",
+            "heighest_japan",
+            "heighest_universal",
+            "lowest_japan",
+            "lowest_universal",
+            "artist",
+            "heighest",
+            "lowest",
+        ]
