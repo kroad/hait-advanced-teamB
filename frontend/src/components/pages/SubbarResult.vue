1d0425ac4c53af2581b16314da3fe667e2b06c97
taillong <63985597+taillong@users.noreply.github.com>
2020-10-31 22:31:43 +0900

    今日はここまで

--- frontend/src/components/pages/SubbarResult.vue
+++ frontend/src/components/pages/SubbarResult.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="result-wrapper">
     <v-list color="blue" dark>
-      <v-list-item v-for="artist in artists" :key="artist" link>
+      <!-- ここにrouter-linkを追加 -->
+      <v-list-item v-for="(artist, index) in artists" :key="artist" link>
+        <v-list-item-icon>
+          <v-icon>{{ index + 1 }}</v-icon>
+        </v-list-item-icon>
         <v-list-item-content>
           <v-list-item-title>{{ artist }}</v-list-item-title>
         </v-list-item-content>
