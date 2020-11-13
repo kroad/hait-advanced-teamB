b4e0189ead0e136b354852c994899ccdfcd22a95
taillong <63985597+taillong@users.noreply.github.com>
2020-11-13 20:04:32 +0900

    Update views.py

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,27 +1,27 @@
-from rest_framework import views, status, generics
+from rest_framework import status, generics
 from rest_framework.response import Response
 
-from karaoke.models import Song
+from karaoke.models import Song, Voice
 from .serializers import SongSerializer, VoiceSerializer
 
 from django.db.models import Q
 
 from .machine_learning.predict import predict
-import os
 from config import settings
 
 
 class MlAPIView(generics.ListCreateAPIView):
     """音声ファイルを機械学習に通すAPIクラス"""
 
-    serializer_class = SongSerializer
-    queryset = Song.objects.all()
+    serializer_class = VoiceSerializer
+    queryset = Voice.objects.all()
 
     def create(self, request):
         voiceSerializer = VoiceSerializer(data=request.data)
         voiceSerializer.is_valid(raise_exception=True)
         voiceSerializer.save()
         result = predict(settings.BASE_DIR + voiceSerializer.data["file"])
+        print(result)
         artists = list(result.keys())
         quesryset = Song.objects.filter(
             Q(artist__name=artists[0])
