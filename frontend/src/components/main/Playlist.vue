2c77351efff2b83edc776b42d9f6b82906fd54aa
tamahassam <tamahassam@gmail.com>
2020-12-13 22:20:58 +0900

    playlist 途中まで

--- frontend/src/components/main/Playlist.vue
+++ frontend/src/components/main/Playlist.vue
@@ -1,29 +1,5 @@
 <template>
   <div>
-    <p>Playlist</p>
-    <p>{{ response }}</p>
+    aaa
   </div>
 </template>
-<script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      response: "",
-    };
-  },
-  mounted() {
-    console.log("hello");
-    let vm = this;
-    axios
-      .get("http://127.0.0.1:8000/api/v1/playlist/")
-      .then(function(response) {
-        console.log(response.data);
-        vm.response = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  },
-};
-</script>
