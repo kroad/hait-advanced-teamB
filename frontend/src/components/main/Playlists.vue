3f92aab8ab04d203047e996d92366539c3b6858e
taillong <63985597+taillong@users.noreply.github.com>
2020-12-14 11:15:22 +0900

    Update Playlists.vue

--- frontend/src/components/main/Playlists.vue
+++ frontend/src/components/main/Playlists.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-list>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title> プレイリスト名 </v-list-item-title>
-      </v-list-item-content>
-      <v-list-item-content>
-        <v-list-item-title> 曲数 </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider></v-divider>
-    <v-list-item
-      v-for="playlist in playlists"
-      :key="playlist.id"
-      link
-      :to="'/playlists/' + playlist.name"
-    >
-      <v-list-item-content>
-        {{ playlist.name }}
-      </v-list-item-content>
-      <v-list-item-content> {{ playlist.songs.length }} </v-list-item-content>
-    </v-list-item>
-  </v-list>
+  <v-container>
+    <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title> プレイリスト名 </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-list-item-title> 曲数 </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item
+        v-for="playlist in playlists"
+        :key="playlist.id"
+        link
+        :to="'/playlists/' + playlist.name"
+      >
+        <v-list-item-content>
+          {{ playlist.name }}
+        </v-list-item-content>
+        <v-list-item-content> {{ playlist.songs.length }} </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 <script>
 import axios from "axios";
@@ -31,14 +33,15 @@ export default {
     };
   },
   mounted() {
+    console.log("mounted");
     let vm = this;
     axios
       .get("http://127.0.0.1:8000/api/v1/playlist/")
-      .then(function(response) {
+      .then(function (response) {
         console.log(response.data.results);
         vm.playlists = response.data.results;
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   },
